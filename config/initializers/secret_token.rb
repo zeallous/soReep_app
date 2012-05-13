7a6b28a3dfaf9cb88dbada5c52ff15789e78828f
Anson Zeall <anson.zeall@gmail.com>
2012-05-13 18:19:49 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoReepApp::Application.config.secret_token = '558bb5731e42c25af082a40fdcb3108c02f0e08f8f01d9b10d9bdf004fff7f01924d4bb6c08dbd2c42896aff6a426e7863179c4d3470b4ef6893016b9b1c9a54'
